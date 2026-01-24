a81cdafc3f92c5204cfe23212872bb55768dfeea
Tilak-Kateghar <kateghartilak21@gmail.com>
2026-01-24 18:06:46 +0530

    fix: PORT Configuration

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const port = 8080;
+const port = process.env.PORT;
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
